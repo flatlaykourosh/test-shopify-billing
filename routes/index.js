79ebb1a479e19fa47784ea28c55350379eddbd71
kpasargad <kpasargad2@gmail.com>
2020-05-24 06:17:40 +0430

    Update shopifyBilling Endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var _apiService = require('./../services/apiService')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/shopifyBilling", async (req, res, next) => {
+  const charge_id = req.query.charge_id;
+  const merchantID = req.query.merchant_id;
+  const shop_name = req.query.shopname;
+  let retrieve_path = "/admin/api/2020-04/recurring_application_charges/" + charge_id + ".json" + "&shopname=" + shop_name;
+  let activate_path = "/admin/api/2020-04/recurring_application_charges/#" + id + "/activate.json" + "&shopname=" + shop_name;
+
+  let charge = await _apiService.getShopify(retrieve_path);
+  if (charge) {
+    if (charge.recurring_application_charge.status == "accepted") {
+      //charge is accepted.
+      let activate_res = await _apiService.postShopify(activate_path, charge);
+      if (activate_res) {
+        if (activate_res.recurring_application_charge.status == "activated") {
+          //charge is activated
+          //update subscription plan for merchantID
+          res.redirect("/dashboard");
+        }
+        else {
+          //activation failed
+          res.send(activate_res);
+        }
+      }
+      else {
+        //activation failed
+        res.send(charge)
+      }
+    }
+    else {
+      //charge is not accepted
+      res.send(charge);
+    }
+  }
+  else {
+    //retrieving charge status failed
+  }
 });
 
 module.exports = router;
