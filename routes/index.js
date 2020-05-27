844e5a4f501f793af3e4541723826a55404577db
kpasargad <kpasargad2@gmail.com>
2020-05-27 20:40:56 +0430

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get("/shopifyBilling", async (req, res, next) => {
   const merchantID = req.query.merchant_id;
   const shop_name = req.query.shopname;
   let retrieve_path = "/admin/api/2020-04/recurring_application_charges/" + charge_id + ".json" + "&shopname=" + shop_name;
-  let activate_path = "/admin/api/2020-04/recurring_application_charges/#" + id + "/activate.json" + "&shopname=" + shop_name;
+  let activate_path = "/admin/api/2020-04/recurring_application_charges/" + charge_id + "/activate.json" + "&shopname=" + shop_name;
 
   let charge = await _apiService.getShopify(retrieve_path);
   if (charge) {
